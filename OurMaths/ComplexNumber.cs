5219ba0f96b0271cbec2e7b35217fc19cdc5a194
higgi1 <david.nak@studium.uni-hamburg.de>
2018-03-14 22:37:55 +0100

    XML-dokumentation der ComplexNumber Klasse.

--- OurMaths/ComplexNumber.cs
+++ OurMaths/ComplexNumber.cs
@@ -1,65 +1,134 @@
 ﻿using System;
 namespace OurMaths
 {
+    /// <summary>
+    /// The class ComplexNumbers provides a complex data type and defines the basic calculation operations for them.
+    /// </summary>
+    /// <remarks>
+    /// A ComplexNumber consists of two double values representing the real and the imaginary part.
+    /// </remarks>
     public struct ComplexNumber
     {
         double rr, ii;
+
+        /// <summary>
+        /// Defines the number i as a complex public constant.
+        /// </summary>
         public static ComplexNumber ImaginaryOne = new ComplexNumber(0, 1);
+
+        /// <summary>
+        /// Defines the number 1 as a complex public constant.
+        /// </summary>
         public static ComplexNumber One = new ComplexNumber(1, 0);
+
+        /// <summary>
+        /// Defines the number 0 as a complex public constant.
+        /// </summary>
         public static ComplexNumber Zero = new ComplexNumber(0, 0);
 
+        /// <summary>
+        /// Defines the ComplexNumber.
+        /// </summary>
+        /// <param name="realPart">Double value to be the real part.</param>
+        /// <param name="imaginaryPart">Double value to be the imaginary part.</param>
         public ComplexNumber(double realPart, double imaginaryPart)
         {
             rr = realPart;
             ii = imaginaryPart;
         }
 
-        // Method on Instances
-
-        //imaginary part
+        /// <returns>
+        /// The imaginary part of the current complex number.
+        /// </returns>
         public double imaginaryPart() => this.ii;
 
-        //imaginary part
+        /// <returns>
+        /// The real part of the current complex number.
+        /// </returns>
         public double realPart() => this.rr;
 
-        // adds a complex number
+        /// <returns>
+        /// The sum of the current and another complex number.
+        /// </returns>
+        /// <param name="c">Complex number to be added.</param>
         public ComplexNumber Add(ComplexNumber c) => new ComplexNumber(this.rr + c.rr, this.ii + c.ii);
 
-        // substracts a complex number from the current one
+        /// <returns>
+        /// The difference of the current and another complex number.
+        /// </returns>
+        /// <param name="c">Complex number to be substracted.</param>
         public ComplexNumber Substract(ComplexNumber c) => new ComplexNumber(this.rr - c.rr, this.ii - c.ii);
-        
-        // multiplies a complex number to the current one
+
+        /// <returns>
+        /// The product of the current and another complex number.
+        /// </returns>
+        /// <param name="c">Complex number to be multiplicated.</param>
         public ComplexNumber Multiply(ComplexNumber c) => new ComplexNumber(this.rr * c.rr - this.ii * c.ii, this.rr * c.ii + this.ii * c.rr);
 
-        // divides this complex number through another one
+        /// <returns>
+        /// The quotient of the current and another complex number.
+        /// </returns>
+        /// <param name="c">Complex number to be devided by.</param>
         public ComplexNumber Divide(ComplexNumber c) => new ComplexNumber((this.rr * c.rr + this.ii * c.ii) / (c.rr * c.rr + c.ii * c.ii), (this.ii * c.rr - this.rr * c.ii) / (c.rr * c.rr + c.ii * c.ii));
-       
-        // retruns the norm/absolute value
+
+        /// <returns>
+        /// The norm of the current complex number.
+        /// </returns>
         public double Norm() => Math.Sqrt(this.rr * this.rr + this.ii * this.ii);
+
+        /// <returns>
+        /// The absolute of the current complex number.
+        /// </returns>
         public double Abs() => Math.Sqrt(this.rr * this.rr + this.ii * this.ii);
 
-        // retuns the complex argument
+        /// <returns>
+        /// The complex argument of the current complex number.
+        /// </returns>
         public double Arg() => Math.Atan2(this.ii, this.rr);
 
-        // returns the complex conjugate
+        /// <returns>
+        /// The complex conjugated of the current complex number.
+        /// </returns>
         public ComplexNumber Conjugate() => new ComplexNumber(this.rr, -this.ii);
 
 
-        // opereators
-
-        // Add two complex numbers
+        /// <summary>
+        /// Defines the + operator for complex numbers.
+        /// </summary>
+        /// <param name="c1">First summand.</param>
+        /// <param name="c2">Second summand.</param>
+        /// <returns>The sum of the parameter values.</returns>
         public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2) => new ComplexNumber(c1.rr + c2.rr, c1.ii + c2.ii);
 
-        // Subtract two complex numbers
+        /// <summary>
+        /// Defines the - operator for complex numbers.
+        /// </summary>
+        /// <param name="c1">Minuend.</param>
+        /// <param name="c2">SUbtrahend.</param>
+        /// <returns>The difference of the parameter values.</returns>
         public static ComplexNumber operator -(ComplexNumber c1, ComplexNumber c2) => new ComplexNumber(c1.rr - c2.rr, c1.ii - c2.ii);
 
-        // negate a complex number
+        /// <summary>
+        /// Defines the - operator for only one complex number.
+        /// </summary>
+        /// <param name="c">Value to be negated.</param>
+        /// <returns>The negation of the parameter value.</returns>
         public static ComplexNumber operator -(ComplexNumber c) => new ComplexNumber(-c.rr, -c.ii);
 
-        // multiply two complex numbers
+        /// <summary>
+        /// Defines the * operator for complex numbers.
+        /// </summary>
+        /// <param name="c1">First factor.</param>
+        /// <param name="c2">Second factor.</param>
+        /// <returns>The product of the parameter values.</returns>
         public static ComplexNumber operator *(ComplexNumber c1, ComplexNumber c2) => new ComplexNumber(c1.rr * c2.rr - c1.ii * c2.ii, c1.rr * c2.ii + c1.ii * c2.rr);
-       
-        // divide two complex numbers
+
+        /// <summary>
+        /// Defines the / operator for complex numbers.
+        /// </summary>
+        /// <param name="c1">Dividend.</param>
+        /// <param name="c2">Divisor.</param>
+        /// <returns>The quotient of the parameter values.</returns>
         public static ComplexNumber operator / (ComplexNumber c1, ComplexNumber c2) => new ComplexNumber((c1.rr * c2.rr + c1.ii * c2.ii) / (c2.rr * c2.rr + c2.ii * c2.ii),(c1.ii * c2.rr - c1.rr * c2.ii) / (c2.rr * c2.rr + c2.ii * c2.ii));
        
         // Implicit type conversions
@@ -68,16 +137,19 @@ namespace OurMaths
 
 
 
-
-
-
-        //real part of a complex Number
+        /// <returns></returns>
+        /// <param name="c">Complex number whose real part is desired.</param>
         public static double realPart(ComplexNumber c) => c.realPart();
 
-        //imaginary part of a Complex Number
+        /// <returns></returns>
+        /// <param name="c">Complex number whose imaginary part is desired.</param>
         public static double imaginaryPart(ComplexNumber c) => c.imaginaryPart();
 
-        //exponential of c, defined by exp(c) = exp(Re(c))*(cos(Im(c)) + i*sin(Im(c)))
+        /// <summary>
+        /// Computes the exponantial of a complex number.
+        /// </summary>
+        /// <param name="c">Complex exponent.</param>
+        /// <returns>e to the power of the complex parameter.</returns>
         public static ComplexNumber Exp(ComplexNumber c) => Math.Exp(c.rr)*(new ComplexNumber(Math.Cos(c.ii), Math.Sin(c.ii)));
 
 
